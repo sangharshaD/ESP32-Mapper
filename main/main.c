3de67145687e510cd8ca9ed59d77ec4722092b41
sangharshaD <118693960+sangharshaD@users.noreply.github.com>
2025-05-23 23:30:40 -0500

    Beginning

---
+++ main/main.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+
+void app_main(void)
+{
+
+    while (1) {
+        
+    }
+}
