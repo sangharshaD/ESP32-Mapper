a74a80d016a10046f6ee72c21adb223a1ff02aee
sangharshaD <118693960+sangharshaD@users.noreply.github.com>
2025-05-26 19:06:23 -0500

    Motor Drive Implementation

--- main/main.c
+++ main/main.c
@@ -9,8 +9,60 @@
 // ULTRASONIC SENSOR PINS
 #define USS_ECHO GPIO_NUM_17
 #define USS_TRIG GPIO_NUM_16
+
+// DC MOTOR DRIVER PINS
+#define PWN_IN1 GPIO_NUM_23 // A
+#define PWN_IN2 GPIO_NUM_25
+#define PWN_IN3 GPIO_NUM_26 // B
+#define PWN_IN4 GPIO_NUM_27
+
+#define PWM_FREQ_HZ       1000
+#define PWM_RES_BITS      LEDC_TIMER_8_BIT
+#define PWM_MODE          LEDC_HIGH_SPEED_MODE
+
+// LEDC CONFIG FOR L298N H BRIDGE DRIVER MODULE
+ledc_timer_config_t TIMER = {
+        .speed_mode = LEDC_HIGH_SPEED_MODE,
+        .timer_num = LEDC_TIMER_0,
+        .duty_resolution = PWM_RES_BITS,
+        .freq_hz = PWM_FREQ_HZ,
+        .clk_cfg = LEDC_AUTO_CLK
+};
+ledc_channel_config_t CHANNEL1 = { // OUT 1
+        .gpio_num = PWN_IN1,
+        .speed_mode = LEDC_HIGH_SPEED_MODE,
+        .channel = LEDC_CHANNEL_0,
+        .timer_sel = LEDC_TIMER_0,
+        .duty = 0,
+        .hpoint = 0
+};
+ledc_channel_config_t CHANNEL2 = { // OUT 2
+        .gpio_num = PWN_IN2,
+        .speed_mode = LEDC_HIGH_SPEED_MODE,
+        .channel = LEDC_CHANNEL_1,
+        .timer_sel = LEDC_TIMER_0,
+        .duty = 0,
+        .hpoint = 0
+};
+ledc_channel_config_t CHANNEL3 = { // OUT 3
+        .gpio_num = PWN_IN3,
+        .speed_mode = LEDC_HIGH_SPEED_MODE,
+        .channel = LEDC_CHANNEL_2,
+        .timer_sel = LEDC_TIMER_0,
+        .duty = 0,
+        .hpoint = 0
+};
+ledc_channel_config_t CHANNEL4 = { // OUT 4
+        .gpio_num = PWN_IN4,
+        .speed_mode = LEDC_HIGH_SPEED_MODE,
+        .channel = LEDC_CHANNEL_3,
+        .timer_sel = LEDC_TIMER_0,
+        .duty = 0,
+        .hpoint = 0
+};
  
 void setup(void) {
+    gpio_output_enable(GPIO_NUM_23); gpio_set_level(GPIO_NUM_23, 1);
     gpio_output_enable(STP_IN4); gpio_set_level(STP_IN4, 0);
     gpio_output_enable(STP_IN3); gpio_set_level(STP_IN3, 0);
     gpio_output_enable(STP_IN2); gpio_set_level(STP_IN2, 0);
@@ -18,6 +70,12 @@ void setup(void) {
     gpio_input_enable(USS_ECHO); // ECHO PIN INPUT
     gpio_output_enable(USS_TRIG); // TRIG PIN OUTPUT
     gpio_set_level(USS_TRIG, 0); // TRIG PIN LOW
+
+    ledc_timer_config(&TIMER);
+    ledc_channel_config(&CHANNEL1);
+    ledc_channel_config(&CHANNEL2);
+    ledc_channel_config(&CHANNEL3);
+    ledc_channel_config(&CHANNEL4);
 }
 
 void app_main(void)
--- main/peripherals.h
+++ main/peripherals.h
@@ -8,6 +8,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
+#include "driver/ledc.h"
 #include "xtensa/core-macros.h"
 #include "esp_rom_sys.h"
 #include "esp_log.h"
@@ -72,6 +73,42 @@ uint32_t USS_GET_DISTANCE(gpio_num_t ECHO, gpio_num_t TRIG) {
         uint32_t distance_cm = (time_uS * 0.0343) / 2; // Speed of sound and round-trip
         return distance_cm;
     } else { ESP_LOGW("ULTRASONIC_SENSOR", "FAILED_MEASUREMENT"); return 0;}
+}
+
+// DC MOTOR LEFT DRIVE
+void LEFT_DRIVE(gpio_num_t IN1, gpio_num_t IN2, int32_t speed) {
+    if (speed > 0) {
+        // IN1 = PWM, IN2 = LOW
+        ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, speed);
+        ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);
+        gpio_set_level(IN2, 0);
+    } else if (speed < 0) {
+        // IN1 = LOW, IN2 = PWM
+        ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1, -speed);
+        ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1);
+        gpio_set_level(IN1, 0);
+    } else {
+        gpio_set_level(IN1, 0);
+        gpio_set_level(IN2, 0);
+    }
+}
+
+// DC MOTOR RIGHT DRIVE
+void RIGHT_DRIVE(gpio_num_t IN3, gpio_num_t IN4, int32_t speed) {
+    if (speed > 0) {
+        // IN3 = PWM, IN4 = LOW
+        ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_2, speed);
+        ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_2);
+        gpio_set_level(IN4, 0);
+    } else if (speed < 0) {
+        // IN3 = LOW, IN4 = PWM
+        ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_3, -speed);
+        ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_3);
+        gpio_set_level(IN3, 0);
+    } else {
+        gpio_set_level(IN3, 0);
+        gpio_set_level(IN4, 0);
+    }
 }
 
 #endif // PERIPHERALS_H
